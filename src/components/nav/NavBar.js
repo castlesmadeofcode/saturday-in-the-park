1aa3577e650fe8d3d1310a519a2a076ae0eb8574
castle <castlesmadeofcode@gmail.com>
2020-05-21 13:11:53 -0500

    user can now logout

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,27 +1,42 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const NavBar = props => {
+const NavBar = (props) => {
+  const manageLogout = () => {
+    sessionStorage.removeItem("kennywood-token");
+  };
+  return (
+    <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+      <ul className="nav nav-pills nav-fill">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Saturday in the Park
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/myitinerary">
+            My Itinerary
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" onClick={manageLogout} to="/login">
+            Logout
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-    return (
-        <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
-            <ul className="nav nav-pills nav-fill">
-                <li className="nav-item">
-                    <Link className="nav-link" to="/">Saturday in the Park</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/myitinerary">My Itinerary</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/login">Login</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/register">Register</Link>
-                </li>
-            </ul>
-        </nav>
-    )
-}
-
-export default NavBar
+export default NavBar;
